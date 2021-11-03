4f00cabb3dd30cb8814bf0c257e3251c66efeccc
Miguel Galindo <miguelgalindo@new-host-4.home>
2021-11-03 11:47:13 -0400

    rails user scaffold

---
+++ db/migrate/20211103154525_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
+      t.string :profile_picture
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
