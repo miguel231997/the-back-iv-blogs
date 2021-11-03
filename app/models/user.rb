e4fa7399a612249167c491a24811bb7706cf1cfd
Miguel Galindo <miguelgalindo@new-host-4.home>
2021-11-03 11:57:29 -0400

    Backend user registration done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    has_secure_password
+
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_picture, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, length: { minimum: 8 }
 end
